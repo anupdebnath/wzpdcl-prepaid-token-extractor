4efdae10353a67bf25055e0d9a94edd9b59db8c2
Anup Debnath <anupdebnath7@gmail.com>
2025-04-18 13:41:01 +0600

    🔧 chore(deps): add unplugin-icons for icon management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+        autoInstall: true,
+      }),
+    ],
+  },
 });
